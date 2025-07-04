7222b3b0487acef1069a5daa827a87e7ad897f3d
박준혁 <jun201908@naver.com>
2025-07-04 14:27:43 +0900

    final code

--- app/app/settings.py
+++ app/app/settings.py
@@ -104,7 +104,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -113,5 +113,5 @@ AUTH_USER_MODEL = 'users.User'
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
-MEDIA_ROOT='/vol/web/media'
-STATIC_ROOT='/vol/web/static'
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
