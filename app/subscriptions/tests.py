a2ba45e71019bbd2e6af4d924177f027d4364440
박준혁 <jun201908@naver.com>
2025-07-04 16:23:10 +0900

    final code

--- app/subscriptions/tests.py
+++ app/subscriptions/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+from .models import Subscription
 
-# Create your tests here.
+User = get_user_model()
+
+class SubscriptionTestCase(TestCase):
+    def setUp(self):
+        # 초기화
+        self.client = APIClient()
+
+        # 유저 생성
+        self.user1 = User.objects.create_user(username='효종선배', password='효종짱12', email='king@oz.com')
+        self.user2 = User.objects.create_user(username='효종대왕', password='효종짱12', email='king@oz2.com')
+
+        # 유저 1 로그인
+        self.client.force_authenticate(user=self.user1)
+
+    # 구독 기능 확인
+    def test_create_subscription(self):
+        response = self.client.post('/subscriptions/', {'subscribed_to': self.user2.id})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Subscription.objects.filter(subscriber=self.user1, subscribed_to=self.user2).exists())
+
+    # 구독 취소 확인
+    def test_delete_subscription(self):
+        subscription = Subscription.objects.create(subscriber=self.user1, subscribed_to=self.user2)
+        response = self.client.delete(f'/subscriptions/{self.user2.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Subscription.objects.filter(id=subscription.id).exists())
--- app/subscriptions/views.py
+++ app/subscriptions/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Subscription
+from users.models import User
+from .serializers import SubscriptionSerializer
+from rest_framework.exceptions import ValidationError
+from django.db.models import Q
 
-# Create your views here.
+class SubscriptionList(APIView):
+    def post(self, request):
+        subscriber = request.user # 구독을 요청하는 자
+        subscribed_to_id = request.data.get('subscribed_to')
+
+        data = {'subscriber':subscriber.id, 'subscribed_to' : subscribed_to_id}
+
+        serializer = SubscriptionSerializer(data=data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        except ValidationError as e:
+            return Response(e.detail, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SubscriptionDetail(APIView):
+    def delete(self, request, pk):
+        subscriber = request.user
+        subscription = get_object_or_404(
+            Subscription,
+            subscriber=subscriber,
+            subscribed_to=pk
+        )
+        subscription.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
